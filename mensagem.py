7392ae30d04e3a4b3209b5fb9137480993a29645
Thiago dos Santos Vicente <thiago.santosvicente@gmail.com>
2025-09-18 20:56:16 -0300

    Update mensagem.py
    novas adições

--- mensagem.py
+++ mensagem.py
@@ -1,16 +1,20 @@
-
-# Mensagem usada para definir as etapas do pomodoro
+# Mensagem usada para guiar as o estado atual do pomodoro e exibida ao usuário
 estados_ui = {
     "Foco": "Hora de focar",
     "Pausa": "Hora de pausar",
     "Descanso": "Hora de descansar"
 }
 
-# Mensagem que será usada na caixa de mensagem, entre etapas
+# Mensagens exibidas, em caixa de dialogos, entre troca de momentos.
+# Elas têm a função de mostrar que uma etapas
+# A interação será feita para iniciar ou pular um ciclo
 dialogo_mensagem = {
-    "confirmação": {
+    "ação": {
         "titúlo": "Pomodoro",
-        "mensagem": "O tempo acabou.\nDeseja iniciar a próxima etapa?",
-        "opções": ["Sim", "Não"]
+        "Pausar": "O tempo acabou.\nfazer pequeno descanso?",
+        "Focar": "O descanso Acabou.\nDeseja começar a focar?",
+        "Descansar": "O ciclo acabou.\nVamos descansar",
+        "Novo_ciclo": "Deseja começar\num novo ciclo",
+        "opções": ["[1] Sim", "[2] Não", "[3] Pular"
     }
 }
